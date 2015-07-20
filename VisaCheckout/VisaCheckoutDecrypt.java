58e9f58a9a279ec621bb660f4696bde99215c0ab
Calvin Brown <calvin@kairuconsulting.com>
2015-07-20 13:42:10 -0400

    Project Structure Update

---
+++ VisaCheckout/VisaCheckoutDecrypt.java
@@ -0,0 +1,11 @@
+package VisaCheckout;
+
+public class VisaCheckoutDecrypt {
+
+	public static void main(String apiLoginId, String transactionKey) {
+		// TODO Auto-generated method stub
+		System.out.println("Visa Checkout Decrypt Sample");
+
+	}
+
+}
