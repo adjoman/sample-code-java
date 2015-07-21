5b9b4a35847a0fae61e886a00eaf19b5ecdac747
Calvin Brown <calvin@kairuconsulting.com>
2015-07-21 10:01:23 -0400

    Debit Bank Account Implemented

--- PaymentTransactions/DebitBankAccount.java
+++ PaymentTransactions/DebitBankAccount.java
@@ -1,11 +1,85 @@
 package PaymentTransactions;
 
+import java.math.BigDecimal;
+
+import net.authorize.Environment;
+import net.authorize.api.contract.v1.BankAccountType;
+import net.authorize.api.contract.v1.CreateTransactionRequest;
+import net.authorize.api.contract.v1.CreateTransactionResponse;
+import net.authorize.api.contract.v1.EcheckTypeEnum;
+import net.authorize.api.contract.v1.MerchantAuthenticationType;
+import net.authorize.api.contract.v1.MessageTypeEnum;
+import net.authorize.api.contract.v1.PaymentType;
+import net.authorize.api.contract.v1.TransactionRequestType;
+import net.authorize.api.contract.v1.TransactionResponse;
+import net.authorize.api.contract.v1.TransactionTypeEnum;
+import net.authorize.api.controller.CreateTransactionController;
+import net.authorize.api.controller.base.ApiOperationBase;
+
 public class DebitBankAccount {
 
 	public static void main(String apiLoginId, String transactionKey) {
 		// TODO Auto-generated method stub
 		System.out.println("Debit Bank Account Sample");
 		
+		//Common code to set for all requests
+        ApiOperationBase.setEnvironment(Environment.SANDBOX);
+
+        MerchantAuthenticationType merchantAuthenticationType  = new MerchantAuthenticationType() ;
+        merchantAuthenticationType.setName(apiLoginId);
+        merchantAuthenticationType.setTransactionKey(transactionKey);
+        
+        ApiOperationBase.setMerchantAuthentication(merchantAuthenticationType);
+
+        // Populate the payment data
+        PaymentType paymentType 	= new PaymentType();
+        BankAccountType bankAccount = new BankAccountType();
+        
+        bankAccount.setAccountNumber("411111");
+        bankAccount.setRoutingNumber("325070760");
+        bankAccount.setEcheckType(EcheckTypeEnum.WEB);
+        bankAccount.setNameOnAccount("Test Name");
+        
+        // define the credit card information
+        paymentType.setBankAccount(bankAccount);
+
+        // Create the payment transaction request
+        TransactionRequestType txnRequest = new TransactionRequestType();
+        txnRequest.setTransactionType(TransactionTypeEnum.AUTH_CAPTURE_TRANSACTION.value());
+        txnRequest.setPayment(paymentType);
+        txnRequest.setAmount(new BigDecimal(500.00));
+
+        // Make the API Request
+        CreateTransactionRequest apiRequest = new CreateTransactionRequest();
+        apiRequest.setTransactionRequest(txnRequest);
+        CreateTransactionController controller = new CreateTransactionController(apiRequest);
+        controller.execute();
+
+        CreateTransactionResponse response = controller.getApiResponse();
+
+        if (response!=null) {
+
+            // If API Response is ok, go ahead and check the transaction response
+            if (response.getMessages().getResultCode() == MessageTypeEnum.OK) {
+
+                TransactionResponse result = response.getTransactionResponse();
+                
+                if (result.getResponseCode().equals("1")) {
+                    System.out.println(result.getResponseCode());
+                    System.out.println("Successful Credit Card Transaction");
+                    System.out.println(result.getAuthCode());
+                    System.out.println(result.getTransId());
+                }
+                else
+                {
+                    System.out.println("Failed Transaction" + result.getResponseCode());
+                }
+            }
+            else
+            {
+                System.out.println("Failed Transaction:  " + response.getMessages().getResultCode());
+            }
+        }
 		
 	}
 
