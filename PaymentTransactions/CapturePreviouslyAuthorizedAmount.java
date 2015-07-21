812d441972f748ccca1039e1b2f576c019a6de49
Calvin Brown <calvin@kairuconsulting.com>
2015-07-21 09:07:30 -0400

    Capture Previously Authorized Amount Implemented

--- PaymentTransactions/CapturePreviouslyAuthorizedAmount.java
+++ PaymentTransactions/CapturePreviouslyAuthorizedAmount.java
@@ -1,12 +1,84 @@
 package PaymentTransactions;
 
+import java.math.BigDecimal;
+
+import net.authorize.Environment;
+import net.authorize.api.contract.v1.CreateTransactionRequest;
+import net.authorize.api.contract.v1.CreateTransactionResponse;
+import net.authorize.api.contract.v1.CreditCardType;
+import net.authorize.api.contract.v1.MerchantAuthenticationType;
+import net.authorize.api.contract.v1.MessageTypeEnum;
+import net.authorize.api.contract.v1.PaymentType;
+import net.authorize.api.contract.v1.TransactionRequestType;
+import net.authorize.api.contract.v1.TransactionResponse;
+import net.authorize.api.contract.v1.TransactionTypeEnum;
+import net.authorize.api.controller.CreateTransactionController;
+import net.authorize.api.controller.base.ApiOperationBase;
+
 public class CapturePreviouslyAuthorizedAmount {
 
 	public static void main(String apiLoginId, String transactionKey, 
 			String TransactionAmount, String TransactionID) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Capture Previously Authorized Amount Sample");
-			
+		
+		//Common code to set for all requests
+        ApiOperationBase.setEnvironment(Environment.SANDBOX);
+
+        MerchantAuthenticationType merchantAuthenticationType  = new MerchantAuthenticationType() ;
+        merchantAuthenticationType.setName(apiLoginId);
+        merchantAuthenticationType.setTransactionKey(transactionKey);
+        
+        ApiOperationBase.setMerchantAuthentication(merchantAuthenticationType);
+
+        // Populate the payment data
+        PaymentType paymentType 	= new PaymentType();
+        CreditCardType creditCard 	= new CreditCardType();
+        
+        // define the credit card information
+        creditCard.setCardNumber("4242424242424242");
+        creditCard.setExpirationDate("0822");
+        paymentType.setCreditCard(creditCard);
+
+        // Create the payment transaction request
+        TransactionRequestType txnRequest = new TransactionRequestType();
+        txnRequest.setTransactionType(TransactionTypeEnum.PRIOR_AUTH_CAPTURE_TRANSACTION.value());
+        txnRequest.setPayment(paymentType);
+        txnRequest.setRefTransId(TransactionID);
+        txnRequest.setAmount(new BigDecimal(TransactionAmount));
+
+        // Make the API Request
+        CreateTransactionRequest apiRequest = new CreateTransactionRequest();
+        apiRequest.setTransactionRequest(txnRequest);
+        CreateTransactionController controller = new CreateTransactionController(apiRequest);
+        controller.execute();
+
+        CreateTransactionResponse response = controller.getApiResponse();
+
+        if (response!=null) {
+
+            // If API Response is ok, go ahead and check the transaction response
+            if (response.getMessages().getResultCode() == MessageTypeEnum.OK) {
+
+                TransactionResponse result = response.getTransactionResponse();
+                
+                if (result.getResponseCode().equals("1")) {
+                    System.out.println(result.getResponseCode());
+                    System.out.println("Successful Credit Card Transaction");
+                    System.out.println(result.getAuthCode());
+                    System.out.println(result.getTransId());
+                }
+                else
+                {
+                    System.out.println("Failed Transaction" + result.getResponseCode());
+                }
+            }
+            else
+            {
+                System.out.println("Failed Transaction:  " + response.getMessages().getResultCode());
+            }
+        }
+
 	}
 
 }
